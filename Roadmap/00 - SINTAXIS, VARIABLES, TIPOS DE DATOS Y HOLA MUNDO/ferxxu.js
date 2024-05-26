44a051bf1c02455fc2280e400f722d37a9a472f0
ferxxu <165027823+ferxxu@users.noreply.github.com>
2024-05-26 19:13:59 -0300

    #00-ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ferxxu.js
@@ -0,0 +1,19 @@
+// Hola soy nuevo programando
+// y voy a ser el mejor
+
+// https://developer.mozilla.org/es/docs/Learn/Getting_started_with_the_web/JavaScript_basics
+
+
+var Palabra_Magica = "Porfavor";
+var Palabra_Magica= "Gracias";
+console.log (Palabra_Magica);
+
+
+var lenguaje = "JavaScript :O!";
+var palabra = "string";
+var numero = 7;
+var soltero = true;
+var indefinida = undefined;
+var none = null;
+
+console.log ("Hola, " + lenguaje);
